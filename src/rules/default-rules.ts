760183fa535740e5fe1b2536a9e6092f8268e30a
Tabaran Vadim <vadik4646@gmail.com>
2017-03-28 16:09:25 +0300

    default validate rule fix

--- src/rules/default-rules.ts
+++ src/rules/default-rules.ts
@@ -3,7 +3,6 @@ export let rules = [
   {
     name: 'required',
     validate: function(next, value) {
-      console.log('required valida');
       next(!!value);
     },
     message: 'Field %attributes["v-name"]% is required'
